76c5734c68b0faef4214f95b8c77bdb7bc556c1f
apuitiza <anthony.puitiza.02@gmail.com>
2018-05-02 16:39:42 -0500

    Ahora el cardView tiene la accion del onCliclistener

--- app/src/main/java/com/example/apuitiza/consumewebservice_from_visualstudio/Adapter/CustomerRecycleradapter.java
+++ app/src/main/java/com/example/apuitiza/consumewebservice_from_visualstudio/Adapter/CustomerRecycleradapter.java
@@ -1,12 +1,15 @@
 package com.example.apuitiza.consumewebservice_from_visualstudio.Adapter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.apuitiza.consumewebservice_from_visualstudio.Models.Customers;
 import com.example.apuitiza.consumewebservice_from_visualstudio.R;
@@ -16,11 +19,11 @@ import java.util.List;
 public class CustomerRecycleradapter extends RecyclerView.Adapter<CustomerRecycleradapter.CustomersViewHolder>{
 
     private List<Customers> lstCustomer;
-    private Activity activity;
+    private Context mContext;
 
-    public CustomerRecycleradapter(List<Customers> lstCustomer, Activity activity) {
+    public CustomerRecycleradapter(List<Customers> lstCustomer, Context context) {
         this.lstCustomer = lstCustomer;
-        this.activity = activity;
+        this.mContext = context;
     }
 
     @NonNull
@@ -32,11 +35,17 @@ public class CustomerRecycleradapter extends RecyclerView.Adapter<CustomerRecycl
 
     @Override
     public void onBindViewHolder(CustomersViewHolder holder, int position) {
-        Customers cliente = lstCustomer.get(position);
+        final Customers cliente = lstCustomer.get(position);
         holder.city.setText(cliente.getCity());
         holder.companyName.setText(cliente.getCompanyName());
         holder.idCustomer.setText(cliente.getCustomerID());
         //Recuerda que si quieres crear listView de un ListView aqui se hace
+        holder.layout_item_client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,"click "+ cliente.getCompanyName(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -46,14 +55,15 @@ public class CustomerRecycleradapter extends RecyclerView.Adapter<CustomerRecycl
 
     class CustomersViewHolder extends RecyclerView.ViewHolder {
         private TextView city, companyName, idCustomer;
+        CardView layout_item_client;
 
         public CustomersViewHolder(View itemView) {
             super(itemView);
 
-            city = (TextView) itemView.findViewById(R.id.city);
-            companyName = (TextView) itemView.findViewById(R.id.companyName);
-            idCustomer = (TextView) itemView.findViewById(R.id.idCustomer);
-
+            city =  itemView.findViewById(R.id.city);
+            companyName = itemView.findViewById(R.id.companyName);
+            idCustomer =  itemView.findViewById(R.id.idCustomer);
+            layout_item_client =  itemView.findViewById(R.id.layout_item_client);
         }
     }
 }
