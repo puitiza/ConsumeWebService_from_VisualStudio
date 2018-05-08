bf9dbd2485f957bdcfb903c16d4ef4058175568b
apuitiza <anthony.puitiza.02@gmail.com>
2018-05-08 17:21:17 -0500

    Actualizacion

--- app/src/main/java/com/example/apuitiza/consumewebservice_from_visualstudio/Activities/MainActivity.java
+++ app/src/main/java/com/example/apuitiza/consumewebservice_from_visualstudio/Activities/MainActivity.java
@@ -4,26 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.apuitiza.consumewebservice_from_visualstudio.Adapter.CustomerRecycleradapter;
-import com.example.apuitiza.consumewebservice_from_visualstudio.Events.ErrorEvent;
-import com.example.apuitiza.consumewebservice_from_visualstudio.Events.ResultadoEvents;
 import com.example.apuitiza.consumewebservice_from_visualstudio.Models.Customers;
-import com.example.apuitiza.consumewebservice_from_visualstudio.Models.Resultado;
 import com.example.apuitiza.consumewebservice_from_visualstudio.R;
 import com.example.apuitiza.consumewebservice_from_visualstudio.Services.ResultadoService;
-import com.example.apuitiza.consumewebservice_from_visualstudio.Services.ResultadoServiceProvider;
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
 
@@ -33,36 +20,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static  final String TAG = ResultadoServiceProvider.class.getSimpleName();
-
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(this);
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onResultadoEvent(ResultadoEvents event) {
-        String dataRequest = event.getResultado().getGetDataResult();
-//        txtResult.setText(dataRequest);
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onErrorEvent(ErrorEvent event) {
-//        txtResult.setText(null);
-        Toast.makeText(this,event.getTxtError(),Toast.LENGTH_LONG).show();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,22 +50,15 @@ public class MainActivity extends AppCompatActivity {
                     CustomerRecycleradapter recyclerAdapter = new CustomerRecycleradapter(lista_customers,MainActivity.this);
                     customerRecycler.setAdapter(recyclerAdapter);
                 }else{
-                    EventBus.getDefault().post(new ErrorEvent("No hay informacion del Resultado disponible"));
+                    Toast.makeText(getApplicationContext(),"No hay informacion del Resultado disponible",Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
             public void onFailure(Call<List<Customers>> call, Throwable t) {
-                Log.e(TAG,"No esta disponible la información");
-                EventBus.getDefault().post(new ErrorEvent("Problema de Internet , No hay informacion del resultado disponible"));
-
+                Toast.makeText(getApplicationContext(),"Problema de Internet , No hay informacion del resultado disponible",Toast.LENGTH_LONG).show();
             }
         });
-    }
-
-    private void requestResultado(String word) {
-        ResultadoServiceProvider result = new ResultadoServiceProvider();
-        result.getResultado(word);
     }
 
 }
